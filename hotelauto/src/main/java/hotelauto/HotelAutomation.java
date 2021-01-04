5086d38ae42cc1a40573f1b034e2ee20b7f2cafe
Sumeet Das <sumeetdas1992@gmail.com>
2021-01-04 18:52:01 +0530

    added new file for Hotel Automation

---
+++ hotelauto/src/main/java/hotelauto/HotelAutomation.java
@@ -0,0 +1,11 @@
+package hotelauto;
+
+import hotelauto.vo.Hotel;
+
+public class HotelAutomation {
+    private final Hotel hotel;
+
+    public HotelAutomation (final Hotel hotel) {
+        this.hotel = hotel;
+    } 
+}
